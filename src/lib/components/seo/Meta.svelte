7c6425131bc0190fce358c2b276e667206acb2c7
Mikeu Dev <rikiruswandi28@gmail.com>
2026-04-30 22:58:02 +0700

    feat: update Open Graph metadata tags and include necessary namespace in app HTML

--- src/lib/components/seo/Meta.svelte
+++ src/lib/components/seo/Meta.svelte
@@ -68,6 +68,9 @@
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
 	<meta property="og:image" content={ogImage} />
+	<meta property="og:image:secure_url" content={ogImage} />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
 	<meta property="og:site_name" content="Kerjao.id" />
 	<meta property="og:locale" content="id_ID" />
 
